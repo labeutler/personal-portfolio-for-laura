1211f774458767fa9d5c50dafe3c0a73153c7821
Laura Beutler <labeutler@gmail.com>
2023-02-23 07:33:55 -0500

    trying to get images to pull for correct area

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,67 +1,92 @@
 // import React, { useState } from "react";
 import React from "react";
+import { removeHyphan } from "../utils/helpers";
 
-function Project(props) {
+// function Project(props) {
     // const [input, setInput] = useState('');
-    return (
-        <h1>Project</h1>,
-        <br></br>,
-        <div>
-            <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
-                {props.projects.map((project) => (
-                    <div className="card">
-                        <div className="card-image">
-                            <figure className="image is-4by3">
-                                <a href={project.live} target="_blank" rel="noreferrer">
-                                    <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
-                                </a>
-                            </figure>
-                        </div>
-                        <div className="card-content">
-                            <div className="media">
-                                <div className="media-left"></div>
-                                <div className="media-content">
-                                    <p className="titel is-4" key={project.id}>
-                                        {project.title}
-                                    </p>
-                                </div>
-                            </div>
+function Project({ project }) {
+    const { name, languages, packages, description, repo, link } = project;
 
-                            <div className="content hast-text-left">
-                                {project.description}
-                                <br />
-                                <br />
-                                <div className="content is-family-code">
-                                    Languages: {project.languages}
-                                    <br />
-                                    NPM packages: {project.packages}
-                                </div>
-                                <div className="card">
-                                    <footer className="card-footer">
-                                        <a
-                                            href={project.repo}
-                                            className="card-footer-item"
-                                            target="_blank" rel="noreferrer"
-                                        >
-                                            See the Repo!
-                                        </a>
-                                        <br />
-                                        <a
-                                            href={project.live}
-                                            className="card-footer-item"
-                                            target="_blank" rel="noreferrer"
-                                        >
-                                            See the Live Site!
-                                        </a>
-                                    </footer>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </div>
+    return (
+        <div className="project" key={name}>
+            <img
+            src={require(`../assets/projects/${name}.png`).default}
+            alt={removeHyphan(name)}
+            className="project-bg"
+        />
+        <div className="project-text">
+            <h3>
+                <a href={link}>{removeHyphan(name)}</a>{' '}
+                <a href={repo}>
+                    <i className="fab fa-github"></i>
+                </a>
+            </h3>
+            <p>{languages}</p>
+            <br />
+            <p>{packages}</p>
+            <br />
+            <p>{description}</p>
+            <br />
         </div>
-    )
+        </div>
+        // <h1>Project</h1>,
+        // <br></br>,
+        // <div>
+        //     <div className="columns is-desktop is-justify-content-center is-flex-wrap-wrap is-flex-direction-row">
+        //         {props.projects.map((project) => (
+        //             <div className="card">
+        //                 <div className="card-image">
+        //                     <figure className="image is-4by3">
+        //                         <a href={project.live} target="_blank" rel="noreferrer">
+        //                             <img src={process.env.PUBLIC_URL + project.image} alt="Placeholder image" />
+        //                         </a>
+        //                     </figure>
+        //                 </div>
+        //                 <div className="card-content">
+        //                     <div className="media">
+        //                         <div className="media-left"></div>
+        //                         <div className="media-content">
+        //                             <p className="titel is-4" key={project.id}>
+        //                                 {project.title}
+        //                             </p>
+        //                         </div>
+        //                     </div>
+
+        //                     <div className="content hast-text-left">
+        //                         {project.description}
+        //                         <br />
+        //                         <br />
+        //                         <div className="content is-family-code">
+        //                             Languages: {project.languages}
+        //                             <br />
+        //                             NPM packages: {project.packages}
+        //                         </div>
+        //                         <div className="card">
+        //                             <footer className="card-footer">
+        //                                 <a
+        //                                     href={project.repo}
+        //                                     className="card-footer-item"
+        //                                     target="_blank" rel="noreferrer"
+        //                                 >
+        //                                     See the Repo!
+        //                                 </a>
+        //                                 <br />
+        //                                 <a
+        //                                     href={project.live}
+        //                                     className="card-footer-item"
+        //                                     target="_blank" rel="noreferrer"
+        //                                 >
+        //                                     See the Live Site!
+        //                                 </a>
+        //                             </footer>
+        //                         </div>
+        //                     </div>
+        //                 </div>
+        //             </div>
+        //         ))}
+        //     </div>
+        // </div>
+    );
 }
 
 export default Project;
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,86 +1,104 @@
-import React from "react";
+import React, { useState } from "react";
 import Project from "../Project";
+// import Image from "../../assets/projects/"
+// import aTechiesBlog from "../../assets/projects/a_techies_blog.png";
+function Portfolio() {
+    const [projects] = useState([
 
-const projects = [
+    // const projects = [
     {
         id: 0,
-        title: "",
-        languages: "",
-        packages: "",
+        name: "Portfolio",
+        languages: "everything",
+        packages: "json, express",
         image: "",
-        description: "",
-        repo: "",
-        live: "",
+        description: "my personal portfolio",
+        repo: "https://github.com/labeutler/potential_react_candidates",
+        link: "",
+
     },
     {
         id: 1,
-        title: "A Techies Blog",
+        name: "A-Techies-Blog",
         languages: "html, handlebars, js",
         packages: "bcrypt, connect-session-sequelize,dotenv, Express.js, express-handlebars, express-session, fetch-schema, mysql2, schema, sequelize",
-        image: "/image/proj1.png",
+        // image: {aTechiesBlog},
         description: "A place where techies can go to post blogs, and read what others have written",
         repo: "https://github.com/labeutler/a_techies_blog",
-        live: "https://a-techies-blog.herokuapp.com/",
+        link: "https://a-techies-blog.herokuapp.com/",
     },
     {
         id: 2,
-        title: "5 Day Weather Forcast",
+        name: "5 Day Weather Forcast",
         languages: "html, js, css, bootstrap",
         packages: "none",
-        image: "/image/proj2.png",
+        // image: "/image/proj2.png",
         description: "An app allowing you to check the current local weather and 5 day forecast.  Also, stores previous cities searched.",
         repo: "https://github.com/labeutler/5-day-weather-forecast/blob/main/index.html",
-        live: "https://labeutler.github.io/5-day-weather-forecast/",
+        link: "https://labeutler.github.io/5-day-weather-forecast/",
     },
     {
         id: 3,
-        title: "Internet Free Text Editor",
+        name: "Internet Free Text Editor",
         languages: "node, js, html, routes, json ",
         packages: "Express, if-env, nodemon",
-        image: "/image/proj3.png",
+        // image: "/image/proj3.png",
         description: "Being able to edit notes when either on or off the internet using PWA (Progressive Web Applications).",
         repo: "https://github.com/labeutler/internet_free_text_editor",
-        live: "https://internet-free-text-editor.herokuapp.com/",
+        link: "https://internet-free-text-editor.herokuapp.com/",
     },
     {
         id: 4,
-        title: "Cocktail Music Match",
+        name: "Cocktail Music Match",
         languages: "html, tailwinds, css, js ",
         packages: "none",
-        image: "/image/proj4.png",
+        // image: "/image/proj4.png",
         description: "Have a Cocktail with a recipe generated with a game to play while I enjoy it.",
         repo: "https://github.com/CocktailMusicMatch/CocktailMusicMatch",
-        live: "https://cocktailmusicmatch.github.io/CocktailMusicMatch/",
+        link: "https://cocktailmusicmatch.github.io/CocktailMusicMatch/",
     },
     {
         id: 5,
-        title: "Task Organizer",
+        name: "Task Organizer",
         languages: "Routes, html, css, js, db.json, node",
         packages: "Express, uuid",
-        image: "/image/proj5.png",
+        // image: "/image/proj5.png",
         description: "The ability to organize thoughts and keep track of them until they are complete.",
         repo: "https://git.heroku.com/internet-free-text-editor.git",
-        live: "https://sleepy-tor-26670.herokuapp.com/",
+        link: "https://sleepy-tor-26670.herokuapp.com/",
     },
     {
         id: 6,
-        title: "Portfolio Challenge 2",
+        name: "Portfolio Challenge 2",
         languages: "html, js, css, ",
         packages: "none",
-        image: "/image/proj6.png",
+        // image: "/image/proj6.png",
         description: "A electronic version of my resume for you to review and see what work I have accomplished along the way.",
         repo: "https://github.com/labeutler/lb-project-portfolio",
-        live: "https://labeutler.github.io/lb-project-portfolio/",
+        link: "https://labeutler.github.io/lb-project-portfolio/",
     },
-];
+]);
 
-function Portfolio() {
+// function Portfolio() {
     return (
         <div>
-            <p className="content is-medium">Portfolio</p>
+            <div className="flex-row">
+                <ul>
+                    {projects.map(project => (
+                        <li>{project.name}</li>
+                    ))}
+                </ul>
+                {/* {projects.map((project, idx) => (
+                    <Project
+                    project={project}
+                    key={"project" + idx}
+                    />
+                ))} */}
+            </div>
+            {/* <p className="content is-medium">Portfolio</p>
             <hr />
 
-            <Project projects={projects} />
+            <Project projects={projects} /> */}
         </div>
     );
 }
